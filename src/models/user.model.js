ed6aeb25db44057088bad77e819adc5c184e568c
divya562 <divyabagul2502@gmail.com>
2024-06-15 00:06:52 +0530

    register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,7 +52,7 @@ const userSchema = new mongoose.Schema(
 // Note : we cant use arrow function beause we dont have reference of this
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next()
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 }) 
 
