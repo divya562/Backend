dcaec74bff3b20f11042fe92795249874adbc2a5
divya562 <divyabagul2502@gmail.com>
2024-06-14 21:26:55 +0530

    router and controller

--- src/app.js
+++ src/app.js
@@ -13,4 +13,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"})) // url data encode
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users", userRouter)
+
+
 export {app}
