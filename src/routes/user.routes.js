3d957c76690e9c303134c4f46ef06c51ea0736b7
divya562 <divyabagul2502@gmail.com>
2024-06-15 16:21:29 +0530

    Access Refresh Token, Middleware and cookies in Backend

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 import { Router } from "express";
 import { upload } from "../middleware/multer.middleware.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router()
 
@@ -16,6 +17,15 @@ router.route("/register").post(
         }
     ]),
     registerUser
+)
+
+router.route('/login').post(
+    loginUser
+)
+
+//secured route
+router.route('/logout').post(
+   verifyJWT, logoutUser
 )
 
 export default router
